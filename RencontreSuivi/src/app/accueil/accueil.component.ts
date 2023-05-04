387404c7cb5dcd680954cce1464638af49bf338b
rpaturel <ronan.paturel@student.junia.com>
2023-05-04 10:01:42 +0200

    page principale

--- RencontreSuivi/src/app/accueil/accueil.component.ts
+++ RencontreSuivi/src/app/accueil/accueil.component.ts
@@ -15,13 +15,11 @@ export class AccueilComponent {
 
 (async function() {
   const data = [
-    { year: 2010, count: 10 },
-    { year: 2011, count: 20 },
-    { year: 2012, count: 15 },
-    { year: 2013, count: 25 },
-    { year: 2014, count: 22 },
-    { year: 2015, count: 30 },
-    { year: 2016, count: 28 },
+    { year: 2010, count: 5 },
+    { year: 2011, count: 2 },
+    { year: 2012, count: 0 },
+    { year: 2013, count: 4 },
+    { year: 2014, count: 3 },
   ];
 
   var Courbe = new Chart("Courbe",
